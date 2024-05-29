14387e1c9ef284163b008e28a1cf420840a10b80
NEIN <nowapraim@gmail.com>
2024-05-29 18:06:24 +0500

    test 2

--- src/test.kt
+++ src/test.kt
@@ -2,14 +2,23 @@ import kotlinx.coroutines.*
 import java.io.Console
 
 fun test(list: MutableList<MainTypesInfo>) = runBlocking {
+    val mainL = 20
     var c = 0
     val li = listOf("[ \\ ]", "[ | ]", "[ / ]", "[ - ]")
+    val lls = """
+        grq
+        aSDa
+        aDFsa
+    """.trimIndent()
     val fds = launch(Dispatchers.IO) {
         while (true){
             print("\r")
-            if (c > 3) c=0
-            print(li[c])
-            delay(500)
+            if (c > mainL) break
+            print("[" + "=".repeat(c) + " ".repeat(mainL-c) + "]")
+            //print("\r")
+
+            //print(lls/*li[c]*/)
+            delay(100)
             c++
         }
     }
